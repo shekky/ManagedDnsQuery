4a0fb852b017855dccfbdcc4a2ddedf6d0dbb752
tburnett80 <tim.burnett@newbalance.com>
2014-01-14 19:52:13 -0600

    made sure domain extension is lowercase to ensure match

--- ManagedDnsQuery/WHOIS/Concretes/TldHandler.cs
+++ ManagedDnsQuery/WHOIS/Concretes/TldHandler.cs
@@ -47,7 +47,7 @@ namespace ManagedDnsQuery.WHOIS.Concretes
             if (Map == null || !Map.Any())
                 Map = Parser.Parse(string.Format(@"{0}\TLD_List.txt", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)));
 
-            var peices = domain.Trim().Split('.');
+            var peices = domain.ToLower().Trim().Split('.');
             if (!peices.Any())
                 return null;
 
