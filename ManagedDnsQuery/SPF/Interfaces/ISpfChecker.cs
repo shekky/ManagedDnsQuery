1c76b5921c1414053fcd8baf0346995a9ac8f5b9
tburnett80 <tim.burnett@newbalance.com>
2014-01-24 22:00:19 -0600

    Added Spf Checker Interface and Concrete

---
+++ ManagedDnsQuery/SPF/Interfaces/ISpfChecker.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ManagedDnsQuery.SPF.Interfaces
+{
+    public enum SpfResult
+    {
+        NoResult = 0,
+        Pass = 1,
+        Fail = 2,
+        SoftFail = 3,
+    }
+
+    public interface ISpfChecker
+    {
+        SpfResult VerifySpfRecord(string domain, string ip);
+    }
+}
