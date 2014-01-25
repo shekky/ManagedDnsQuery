05afd7886fcb806b693186e7d0280368e8521b04
tburnett80 <tim.burnett@newbalance.com>
2014-01-25 12:53:54 -0600

    Added string extensions to prevent application from blowing up on null or empty strings

---
+++ ManagedDnsQuery/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ManagedDnsQuery
+{
+    /// <summary>
+    /// Extensions that wont blow up on null or empty strings.
+    /// </summary>
+    internal static class StringExtensions
+    {
+        public static string TryTrim(this string value)
+        {
+            return (string.IsNullOrEmpty(value) ? value : value.Trim());
+        }
+
+        public static string TrySubstring(this string value, int start, int chars)
+        {
+            return ((string.IsNullOrEmpty(value) || value.Length < (start + chars)) ? value : value.Substring(start, chars));
+        }
+
+        public static string TrySubstring(this string value, int start)
+        {
+            return ((string.IsNullOrEmpty(value) || value.Length < (start)) ? value : value.Substring(start));
+        }
+
+        public static string TryToLower(this string value)
+        {
+            return (string.IsNullOrEmpty(value) ? value : value.ToLower());
+        }
+
+        public static string TryToUpper(this string value)
+        {
+            return (string.IsNullOrEmpty(value) ? value : value.ToUpper());
+        }
+    }
+}
